60c0f917c20d0d4bf14c8b840aded2b6817ca69d
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-06-07 09:08:00 -0300

    Adicionando janela principal

---
+++ app/visao/janela_principal.py
@@ -0,0 +1,26 @@
+import tkinter
+
+from controlador.controlador import Controlador
+
+class JanelaPrincipal(tkinter.Tk):
+    def __init__(self, controlador: Controlador, *args, **kwargs):
+        # Chamando o pai
+        tkinter.Tk.__init__(self, *args, **kwargs)
+
+        # Definindo o título
+        self.title("AS05: Implementação de Assistente Conversacional Baseado em LLM")
+
+        # Configurando o tamanho da janela
+        screen_width = 500
+        screen_height = 350
+        self.geometry(f"{screen_width}X{screen_height}+0+0")
+
+        # Barra de menu
+        barra_menu = tkinter.Menu(self)
+
+        # Menu arquivo
+        menu_arquivo = tkinter.Menu(barra_menu, tearoff=0)
+        menu_arquivo.add_command(
+            label="Abrir pasta com arquivos PDF",
+            command=lambda: controlador.abrir_pasta_pdf()
+        )
