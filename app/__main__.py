1b3d7bfa1b2cc9fad0f4ac3fa57e405ecab4fac8
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-06-07 09:24:35 -0300

    Corrigindo controlador

--- app/__main__.py
+++ app/__main__.py
@@ -1,9 +1,9 @@
-from visao.janela_principal import JanelaPrincipal
+from controlador.controlador import Controlador
 
 # Chaves de API
 pinecone_api_key = "pcsk_5k26G8_PPWJijDSULaGXtVLyycFu8vGeiFNxocFGsSijhyMwxUrm1pg8kj5CZF7a21QfWG"
 huggingfacehub_api_key = "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"
 
 if __name__ == '__main__':
-    janela_principal = JanelaPrincipal()
-    janela_principal.mainloop()
+    controlador = Controlador(pinecone_api_key, huggingfacehub_api_key)
+    controlador.janela_principal.mainloop()
