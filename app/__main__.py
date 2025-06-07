edb3e26a2d8b09bfd78bad1bd77b4862a39a12bc
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-06-07 08:53:52 -0300

    Criando main

--- app/__main__.py
+++ app/__main__.py
@@ -1,2 +1,8 @@
+from controlador.controlador import Controlador
 
+# Chaves de API
+pinecone_api_key = ""
+huggingfacehub_api_key = ""
 
+if __name__ == '__main__':
+    controlador = Controlador(pinecone_api_key, huggingfacehub_api_key)
