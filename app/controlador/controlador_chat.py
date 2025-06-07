5c40ca63a2cb2bde18e252644508c0b7b4aa4da8
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-06-07 12:05:17 -0300

    Removendo controlador

--- app/controlador/controlador_chat.py
+++
@@ -1,7 +0,0 @@
-class ControladorChat:
-    def __init__(self, pai):
-        # Guardando o pai
-        self.pai = pai
-
-        # Array que armazena o histórico de mensagens trocadas
-        self.mensagens = []
