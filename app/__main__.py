0d7e993b816ada28fcfb9b1821bc1b8a26b3d6df
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2025-06-24 16:54:44 -0300

    Melhorando a resposta do modelo

--- app/__main__.py
+++ app/__main__.py
@@ -1,9 +1,14 @@
 from controlador.controlador import Controlador
+import nltk
 
 # Chaves de API
 pinecone_api_key = "pcsk_5k26G8_PPWJijDSULaGXtVLyycFu8vGeiFNxocFGsSijhyMwxUrm1pg8kj5CZF7a21QfWG"
 huggingfacehub_api_key = "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"
 
 if __name__ == '__main__':
+    # Baixando o Tokenizer
+    nltk.download('punkt_tab')
+
+    # Executando programa
     controlador = Controlador(pinecone_api_key, huggingfacehub_api_key)
     controlador.janela_principal.mainloop()
